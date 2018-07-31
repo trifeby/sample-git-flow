244357ff01c820523f55068d30b74f609af52b88
Sendaay <rahmamns1@gmail.com>
2018-07-31 14:58:22 +0700

    commit Dev4

---
+++ app/Dev4_Model_1.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Dev4_Model_1 extends Model
+{
+    //
+}
---
+++ app/Http/Controllers/Dev4_1.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Dev4_1 extends Controller
+{
+    //
+}
